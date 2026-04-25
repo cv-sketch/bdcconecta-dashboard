a697653d523e5d66d178fec5e040f444cec721bc
cv-sketch <cv@ibba.group>
2026-04-25 12:53:24 -0300

    feat: protect all routes with auth, add Login and AdminUsuarios

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,57 @@
 import React, { useEffect } from 'react'
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Navigate } from 'react-router-dom'
 import Layout from './components/Layout'
+import ProtectedRoute from './components/ProtectedRoute'
 import Dashboard from './pages/Dashboard'
 import Clientes from './pages/Clientes'
 import Wallets from './pages/Wallets'
 import Transferencias from './pages/Transferencias'
 import Movimientos from './pages/Movimientos'
+import Login from './pages/Login'
+import AdminUsuarios from './pages/AdminUsuarios'
 import { useStore } from './store/useStore'
 
 export default function App() {
   const loadAll = useStore((s) => s.loadAll)
 
-  useEffect(() => {
-    loadAll()
-  }, [loadAll])
+  useEffect(() => { loadAll() }, [loadAll])
 
   return (
-    <Layout>
-      <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/clientes" element={<Clientes />} />
-        <Route path="/wallets" element={<Wallets />} />
-        <Route path="/transferencias" element={<Transferencias />} />
-        <Route path="/movimientos" element={<Movimientos />} />
-      </Routes>
-    </Layout>
+    <Routes>
+      <Route path="/login" element={<Login />} />
+
+      <Route path="/" element={
+        <ProtectedRoute>
+          <Layout><Dashboard /></Layout>
+        </ProtectedRoute>
+      } />
+      <Route path="/clientes" element={
+        <ProtectedRoute>
+          <Layout><Clientes /></Layout>
+        </ProtectedRoute>
+      } />
+      <Route path="/wallets" element={
+        <ProtectedRoute>
+          <Layout><Wallets /></Layout>
+        </ProtectedRoute>
+      } />
+      <Route path="/transferencias" element={
+        <ProtectedRoute>
+          <Layout><Transferencias /></Layout>
+        </ProtectedRoute>
+      } />
+      <Route path="/movimientos" element={
+        <ProtectedRoute>
+          <Layout><Movimientos /></Layout>
+        </ProtectedRoute>
+      } />
+      <Route path="/admin/usuarios" element={
+        <ProtectedRoute soloAdmin>
+          <Layout><AdminUsuarios /></Layout>
+        </ProtectedRoute>
+      } />
+
+      <Route path="*" element={<Navigate to="/" replace />} />
+    </Routes>
   )
 }
