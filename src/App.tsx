41ffd7c91b546729845a80610aaa7d3af95b1500
cv-sketch <cv@ibba.group>
2026-04-25 12:38:09 -0300

    feat: call loadAll() on mount to fetch data from Supabase

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Routes, Route } from 'react-router-dom'
 import Layout from './components/Layout'
 import Dashboard from './pages/Dashboard'
@@ -6,8 +6,15 @@ import Clientes from './pages/Clientes'
 import Wallets from './pages/Wallets'
 import Transferencias from './pages/Transferencias'
 import Movimientos from './pages/Movimientos'
+import { useStore } from './store/useStore'
 
 export default function App() {
+  const loadAll = useStore((s) => s.loadAll)
+
+  useEffect(() => {
+    loadAll()
+  }, [loadAll])
+
   return (
     <Layout>
       <Routes>
